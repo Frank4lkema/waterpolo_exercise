a6b3d6b6bdf0666b866a9e53c33258ac4d7e2538
frank <f.alkema@mobiel.nl>
2023-01-25 19:45:46 +0100

    Removed comment

--- app/javascript/controllers/exercise_controller.js
+++ app/javascript/controllers/exercise_controller.js
@@ -4,7 +4,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   toggle() {
-    // this.element.classList.toggle("hidden")
     document.getElementById("new-exercise-form").classList.toggle("hidden")
   }
 }

53a56649862d44bde2c67a697fbce873ddb083af
frank <f.alkema@mobiel.nl>
2023-01-27 16:54:31 +0100

    Added Training

---
+++ app/controllers/trainings_controller.rb
@@ -0,0 +1,54 @@
+class TrainingsController < ApplicationController
+  before_action :set_training, only: %i[ show edit update destroy ]
+
+  def index
+    @trainings = Training.all
+  end
+
+  def show
+  end
+
+  def new
+    @training = Training.new
+  end
+  def edit
+  end
+  def create
+    @training = Training.new(training_params)
+
+    respond_to do |format|
+      if @training.save
+        format.html { redirect_to training_url(@training), notice: "Training was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @training.update(training_params)
+        format.html { redirect_to training_url(@training), notice: "Training was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @training.destroy
+
+    respond_to do |format|
+      format.html { redirect_to trainings_url, notice: "Training was successfully destroyed." }
+    end
+  end
+
+  private
+    def set_training
+      @training = Training.find(params[:id])
+    end
+
+    def training_params
+      params.require(:training).permit(:trainings_date)
+    end
+end

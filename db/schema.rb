cf2da0fd0ae4689d1246b905268b1d6141ecac27
frank <f.alkema@mobiel.nl>
2023-01-27 16:53:50 +0100

    Added Training

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_25_200858) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_27_155132) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_25_200858) do
     t.integer "type"
     t.string "purpose"
     t.integer "minutes"
+  end
+
+  create_table "trainings", force: :cascade do |t|
+    t.date "trainings_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
